ae7b6c218450e4e1579227f4b233154b4390841e
Arrigoj <arrigoj@outlook.fr>
2021-03-24 17:06:36 +0100

    Recréation du projet Multimedia mais cette fois avec une nouvelle structure de fichier Pour l'instant seul la playlist fonctionne. En cours -> Youtube Search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,54 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { DialogChooseTypeComponent } from './playlist/dialogChooseType/dialog-choose-type.component';
+import { PlaylistService } from './playlist/services/playlist.service';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+/**
+ * Custom angular notifier options
+ */
+
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+
+const customNotifierOptions: NotifierOptions = {
+  position: { horizontal: { position: 'left', distance: 12 },
+    vertical: { position: 'bottom', distance: 12, gap: 10 }
+  },
+  theme: 'material',
+  behaviour: { autoHide: 5000, onClick: 'hide', onMouseover: 'pauseAutoHide', showDismissButton: true, stacking: 1 },
+  animations: { enabled: true, show: { preset: 'slide', speed: 300, easing: 'ease' },
+    hide: { preset: 'fade', speed: 300, easing: 'ease', offset: 50 },
+    shift: { speed: 300, easing: 'ease' },
+    overlap: 150 }
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistComponent,
+    DialogChooseTypeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NotifierModule.withConfig(customNotifierOptions),
+    MatDialogModule,
+    RouterModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: 'playlist', pathMatch: 'full'},
+    ]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    PlaylistService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/playlist/model/types-interface.ts
@@ -0,0 +1,12 @@
+export interface Types {
+  types: string;
+  videoId: string;
+  videoUrl: string;
+  channelId: string;
+  channelUrl: string;
+  channelTitle: string;
+  title: string;
+  publishedAt: Date;
+  description: string;
+  thumbnail: string;
+}
