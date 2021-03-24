b1746fee8e16fd296e0ad4eceb47cefa8f3cda0a
Arrigoj <arrigoj@outlook.fr>
2021-03-24 17:40:52 +0100

    Ajout de Youtube

---
+++ projects/youtube/src/app/search/container/search-container.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { SearchService} from '../../shared/services/search.service';
+import { Video} from '../../shared/models/search.interface';
+
+@Component({
+  selector: 'app-search-container',
+  templateUrl: './search-container.component.html',
+  styleUrls: ['./search-container.component.css']
+})
+export class SearchContainerComponent {
+
+  inputTouched = false;
+  loading = false;
+  videos: Video[] = [];
+
+  constructor(private searchService: SearchService) { }
+
+  handleSearch(inputValue: string) {
+    this.loading = true;
+    this.searchService.getVideos(inputValue)
+      .subscribe((items: any) => {
+        this.videos = items.map(item => {
+          return {
+            title: item.snippet.title,
+            videoId: item.id.videoId,
+            videoUrl: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+            channelId: item.snippet.channelId,
+            channelUrl: `https://www.youtube.com/channel/${item.snippet.channelId}`,
+            channelTitle: item.snippet.channelTitle,
+            description: item.snippet.description,
+            publishedAt: new Date(item.snippet.publishedAt),
+            thumbnail: item.snippet.thumbnails.high.url
+          };
+        });
+        this.inputTouched = true;
+        this.loading = false;
+      });
+  }
+
+}
