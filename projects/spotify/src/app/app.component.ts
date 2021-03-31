c66610c9a5e7b158b39e0f7a0e087419f1e45292
Arrigoj <arrigoj@outlook.fr>
2021-03-31 16:45:19 +0200

    Initialisation du token spotify beaucoup plus tot afin de ne pas attendre la 1er page + Lorsque l'on lance spotify redirection automatique au début sur la page home au lieu d'une page blanche

--- projects/spotify/src/app/app.component.ts
+++ projects/spotify/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {GlobalService} from './services/global.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
   title = 'spotify';
+
+  constructor(private globalService: GlobalService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.globalService.accessToken; //initialise le token
+    this.router.navigate(['/spotify/home']);
+  }
 }
