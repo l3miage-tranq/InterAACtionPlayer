f38ad8971a9b9214fcc9ccefb566f7c15d85aba9
Arrigoj <arrigoj@outlook.fr>
2021-04-23 15:12:52 +0200

    Systeme de languages fait pour Playlist et Youtube

--- projects/youtube/src/app/app.module.ts
+++ projects/youtube/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { YoutubePipe } from './pipe/youtube.pipe';
  */
 
 import { NotifierModule, NotifierOptions } from 'angular-notifier';
+import {TranslateModule} from '@ngx-translate/core';
 
 const customNotifierOptions: NotifierOptions = {
   position: { horizontal: { position: 'left', distance: 12 },
@@ -37,12 +38,13 @@ const providers = [];
     SearchListComponent,
     YoutubePipe
   ],
-  imports: [
-    BrowserModule,
-    NotifierModule.withConfig(customNotifierOptions),
-    HttpClientModule,
-    AppRoutingModule,
-  ],
+    imports: [
+        BrowserModule,
+        NotifierModule.withConfig(customNotifierOptions),
+        HttpClientModule,
+        AppRoutingModule,
+        TranslateModule,
+    ],
   providers: providers,
   exports: [
     YoutubePipe
