ca8f5a569777373e9d57ddbcc832fd110af59511
Arrigoj <arrigoj@outlook.fr>
2021-05-03 10:41:35 +0200

    Permet d'exporter la playlist au format Json

--- src/app/playlist/dialogComponents/saveDialog/save-dialog.component.ts
+++ src/app/playlist/dialogComponents/saveDialog/save-dialog.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import exportFromJSON from 'export-from-json';
+
+/**
+ * Import Services
+ */
 import { SaveService } from '../../../services/save.service';
 import { NotifierService } from 'angular-notifier';
 import { TranslateService } from '@ngx-translate/core';
+import { PlaylistService } from '../../services/playlist.service';
 
 @Component({
   selector: 'app-save-dialog',
@@ -14,7 +20,8 @@ export class SaveDialogComponent implements OnInit {
   constructor(private dialog: MatDialog,
               private saveService: SaveService,
               private notifier: NotifierService,
-              private translate: TranslateService) {
+              private translate: TranslateService,
+              private playlistService: PlaylistService) {
   }
 
   ngOnInit(): void {
@@ -25,6 +32,12 @@ export class SaveDialogComponent implements OnInit {
    * Then close the DialogComponent and notify that the save is done;
    */
   public submit(){
+    exportFromJSON({
+      data: this.playlistService.playList,
+      fields: {} ,
+      fileName: 'MyPlaylist',
+      exportType: exportFromJSON.types.json
+    });
     this.dialog.closeAll();
     this.notifier.notify('warning', this.translate.instant('notifier.save'));
   }
