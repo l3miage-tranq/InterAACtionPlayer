42a3195fc322f7004387819b96dcc3119601ed92
Arrigoj <arrigoj@outlook.fr>
2021-05-04 10:27:30 +0200

    Lance le mode édition si la playlist est vide. (issue #5)

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -99,6 +99,7 @@ export class PlaylistComponent implements OnInit {
    * Allows to know if the theme value has changed
    * Initialize DialogChooseTypeComponent
    * Wait 500ms for the Playlist to get the Playlist from database
+   * At the start, if the playlist is empty then we active the edit mode
    */
   ngOnInit(): void {
     this.themeService.themeObservable.subscribe(value => {
@@ -106,7 +107,7 @@ export class PlaylistComponent implements OnInit {
     })
     new DialogChooseTypeComponent(this.router, this.dialog);
     setTimeout(() => this.playList = this.playlistService.playList ,500 ); // permet de laisser le temps de charger la playlist sauvegarder dans la playlist
-    if (this.playList == []){
+    if (this.playList.length == 0){
       this.goEdit();
     }
   }
