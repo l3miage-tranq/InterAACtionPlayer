0aaf6ff8c161e292dd0d70c828f4777b00774a98
Arrigoj <arrigoj@outlook.fr>
2021-06-17 16:58:29 +0200

    L'utilisateur peut maintenant modifier sa carte en changeant son nom et son image (issue #42)

---
+++ src/app/playlist/dialogComponents/modifyUser/modify-user.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UsersService } from '../../../services/users.service';
+import { ChooseImgComponent } from '../chooseImgUser/choose-img.component';
+
+@Component({
+  selector: 'app-modify-user',
+  templateUrl: './modify-user.component.html',
+  styleUrls: ['./modify-user.component.css']
+})
+export class ModifyUserComponent implements OnInit {
+
+  name = "";
+  errorNameEmpty = false;
+  image = null;
+  errorImgEmpty = false;
+  showImgChoose = false;
+
+  constructor(private translate: TranslateService,
+              private dialog: MatDialog,
+              private usersService: UsersService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * @param event
+   *
+   * Allows to get the new value given by the user
+   */
+  getNewNameUser(event){
+    this.name = event.target.value;
+  }
+
+  /**
+   * @param event
+   *
+   * Get the new image choose by the user
+   */
+  addNewImage(event){
+    const image = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(image)
+    reader.onload = () => {
+      this.image = reader.result;
+    }
+    this.usersService.imgChoose = this.image;
+    this.showImgChoose = true;
+  }
+
+  /**
+   * Launch the chooseImgDialog, that permit to the user to choose a new image
+   */
+  goChooseNewImg(){
+    const chooseImgDialog = this.dialog.open(ChooseImgComponent, {height: '100%'});
+    chooseImgDialog.afterClosed().subscribe(() => {
+      this.image = this.usersService.imgChoose;
+      if (this.image != null){
+        this.showImgChoose = true;
+      }
+    });
+  }
+
+  /**
+   * If the user cancel then close the DialogComponent
+   */
+  public goCancel(){
+    this.dialog.closeAll();
+  }
+
+  /**
+   * If the new name and the new image is not empty, then modify the user
+   * */
+  submit(){
+    if (this.name != ""){
+      this.errorNameEmpty = false;
+      if (this.image != null){
+        this.errorImgEmpty = false;
+        this.usersService.wantModifyUser = true;
+        this.usersService.userToModify.name = this.name;
+        this.usersService.userToModify.thumbnail = this.image;
+        this.dialog.closeAll();
+      } else {
+        this.errorImgEmpty = true;
+      }
+    }else {
+      this.errorNameEmpty = true;
+    }
+  }
+}
