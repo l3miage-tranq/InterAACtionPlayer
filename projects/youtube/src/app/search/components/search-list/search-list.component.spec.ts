ab4e19149a36413feddc7e7606d3f27d8a8d2cc9
Arrigoj <arrigoj@outlook.fr>
2021-07-26 15:43:16 +0200

    Fix test YouTube (issue #71)

--- projects/youtube/src/app/search/components/search-list/search-list.component.spec.ts
+++ projects/youtube/src/app/search/components/search-list/search-list.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchListComponent } from './search-list.component';
+import { NotifierModule } from 'angular-notifier';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SearchListComponent', () => {
   let component: SearchListComponent;
@@ -8,7 +11,8 @@ describe('SearchListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchListComponent ]
+      declarations: [ SearchListComponent ],
+      imports: [ NotifierModule, TranslateModule.forRoot(), HttpClientModule ]
     })
     .compileComponents();
   });
