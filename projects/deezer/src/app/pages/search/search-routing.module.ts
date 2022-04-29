2b3600c4748b1bcd7c2c7a4746cdc974d6d89bb1
PillouMatou <vincent.arnone@hotmail.fr>
2022-04-29 10:28:49 +0200

    try to fix test

--- projects/deezer/src/app/pages/search/search-routing.module.ts
+++ projects/deezer/src/app/pages/search/search-routing.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { SearchComponent } from './search-component/search.component';
-import {RouterTestingModule} from "@angular/router/testing";
 
 const routes: Routes = [
   {
-    path: '',
-    component: SearchComponent
+    path: '/', component: SearchComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), RouterTestingModule],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class SearchRoutingModule { }
